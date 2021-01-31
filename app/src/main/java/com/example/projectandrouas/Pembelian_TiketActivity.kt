3fb90650041f1b9f2a20343fdc112cd792bd5e82
Bobby Pandaportani Purba <boby18ti@mahasiswa.pcr.ac.id>
2021-01-31 17:21:57 +0700

    Commmit Pertama

---
+++ app/src/main/java/com/example/projectandrouas/Pembelian_TiketActivity.kt
@@ -0,0 +1,12 @@
+package com.example.projectandrouas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Pembelian_TiketActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pembelian__tiket)
+    }
+}
